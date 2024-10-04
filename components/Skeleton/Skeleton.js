1af55e0852b84ef53292691bbaed3eea5bccf88c
mdao2211 <superhero21442922@gmail.com>
2024-10-04 08:49:36 +0700

    commit first

---
+++ components/Skeleton/Skeleton.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { motion } from "framer-motion";
+import { ThreeDots } from "react-loader-spinner";
+
+const Skeleton = () => {
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      viewport={{ once: true }}
+      className="flex justify-center items-center">
+      <ThreeDots
+        height="500"
+        width="500"
+        radius="9"
+        color="#16b8b8"
+        ariaLabel="three-dots-loading"
+        wrapperStyle={{}}
+        wrapperClassName=""
+        visible={true}
+      />
+    </motion.div>
+  );
+};
+
+export default Skeleton;
